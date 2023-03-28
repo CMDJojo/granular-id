c269d7f12fcba7223f3486e7f20ab3bd7ba49f73
Jonathan Widén <jonathan.loke.widen@gmail.com>
2023-03-28 03:34:40 +0200

    Version 0.2.0 with readme and some more functions

---
+++ README.md
@@ -0,0 +1,58 @@
+# granular-id
+
+[![Crates.io](https://img.shields.io/crates/v/granular-id.svg)](https://crates.io/crates/granular-id)
+[![Docs.rs](https://docs.rs/granular-id/badge.svg)](https://docs.rs/granular-id)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+This crate provides a data type `GranularId<T>` that can represent ID numbers with arbitrary precision.
+
+## Features
+
+- `GranularId<T>` is a sequence of components of type `T` that can be ordered and compared.
+- Between any two `GranularId<T>`s, there are infinitely many more granular IDs.
+- `GranularId<T>` is best used with any unsized integer type, such as `u8`, `u16`, `u32`, etc.
+- `GranularId<T>` can also be used with any type that implements the appropriate `num_traits`.
+- `GranularId<T>` has methods to access its parent, children, siblings, and other relations in a tree-like structure.
+
+## Example usage
+
+```rust
+// Create a new GranularId from a vec of u8 (id: 1.2.3)
+let id: GranularId<u8> = vec![1, 2, 3].into();
+
+// Get the parent ID (id: 1.2)
+let parent = id.parent();
+assert_eq!(parent, vec![1, 2].into());
+
+// Iterate over the following siblings of 1.2.3
+let mut next_siblings = id.next_siblings();
+// First one is 1.2.4
+assert_eq!(next_siblings.next().unwrap(), vec![1, 2, 4].into());
+// Then, 1.2.5, etc
+assert_eq!(next_siblings.next().unwrap(), vec![1, 2, 5].into());
+assert_eq!(next_siblings.next().unwrap(), vec![1, 2, 6].into());
+
+// Get an iterator over childrens of 1.2.3
+let mut children = id.children();
+// First one is 1.2.3.0
+assert_eq!(children.next().unwrap(), vec![1, 2, 3, 0].into());
+// Then, 1.2.3.1, etc
+assert_eq!(children.next().unwrap(), vec![1, 2, 3, 1].into());
+assert_eq!(children.next().unwrap(), vec![1, 2, 3, 2].into());
+
+// Each parent is always smaller than all of its children
+assert!(parent < id);
+```
+
+## Installation
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+granular-id = "0.2.0"
+```
+
+## License
+
+This project is licensed under the MIT license. See [LICENSE](LICENSE) for more details.
--- src/lib.rs
+++ src/lib.rs
@@ -40,13 +40,17 @@
 
 use std::{cmp, fmt, iter::Iterator};
 
-use num_traits::{Bounded, CheckedAdd, CheckedSub, One};
 use num_traits::bounds::{LowerBounded, UpperBounded};
+use num_traits::{Bounded, CheckedAdd, CheckedSub, One};
 
-/// This is the main data type `GranularId` which represents any ID with arbitrary granularity,
-/// meaning that there is indefinitely many `GranularId`s in-between any two `GranularId`s. See the
-/// crate-level documentation for an introduction.
-#[derive(Debug, Clone, PartialEq, Eq)]
+/// The data type `GranularId` represents any ID which can have an arbitrary granularity,
+/// meaning that there is indefinitely many `GranularId`s in-between any two `GranularId`s. It is
+/// best used with unsized integer types such as `u8`, `u16`, `u32`, `u64` and `usize`, but may be
+/// used with other data types implementing the required
+/// [`num_traits`](https://docs.rs/num-traits/latest/num_traits/index.html) bounds.
+///
+/// An ID basically consists of multiple *components*
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct GranularId<T> {
     id: Vec<T>,
 }
@@ -70,8 +74,8 @@ impl<T: fmt::Display> fmt::Display for GranularId<T> {
 }
 
 impl<T> PartialOrd for GranularId<T>
-    where
-        T: Ord,
+where
+    T: Ord,
 {
     /// Since `GranularId`s does have a total ordering if and only if its type has a total ordering,
     /// this function never returns `None` and just returns the value [`GranularId<T>::cmp`] would
@@ -82,20 +86,33 @@ impl<T> PartialOrd for GranularId<T>
 }
 
 impl<T> Ord for GranularId<T>
-    where
-        T: Ord,
+where
+    T: Ord,
 {
     /// When comparing two [`GranularId`]s, their relative positioning is determined by its first
     /// non-matching component. All ID:s starting with `1` is less than all ID:s starting with a `2`
-    /// and `1.2` is ordered before `1.2.0`. Here is an example of ordered `GranularId`s:
-    /// * `1`
-    /// * `1.0`
-    /// * `1.1`
-    /// * `1.1.0`
-    /// * `1.1.1`
-    /// * `1.1.2`
-    /// * `1.2`
-    /// * `2`
+    /// and `1.2` is ordered before `1.2.0`. Here is an example of some ordered `GranularId`s:
+    ///
+    /// ```rust
+    /// use num_traits::bounds::LowerBounded;
+    /// use granular_id::GranularId;
+    /// let vec: Vec<GranularId<u8>> = vec![
+    ///     vec![1].into(),         // id 1
+    ///     vec![1, 0].into(),      // id 1.0
+    ///     vec![1, 1].into(),      // id 1.1
+    ///     vec![1, 1, 0].into(),   // id 1.1.0
+    ///     vec![1, 1, 1].into(),   // id 1.1.1
+    ///     vec![1, 1, 2].into(),   // id 1.1.2
+    ///     vec![1, 2].into(),      // id 1.2
+    ///     vec![2].into(),         // id 2
+    /// ];
+    /// let mut min = GranularId::min_value();
+    /// for id in vec {
+    ///     assert!(id > min);
+    ///     min = id;
+    /// }
+    /// ```
+    ///
     /// For a [`GranularId`] to have a total ordering, the component type must also have a total
     /// ordering.
     fn cmp(&self, other: &Self) -> cmp::Ordering {
@@ -106,18 +123,18 @@ impl<T> Ord for GranularId<T>
     }
 }
 
-impl<T> LowerBounded for GranularId<T>
-{
+impl<T> LowerBounded for GranularId<T> {
     /// The lower bound for any [`GranularId`] (the smallest ID) is the empty ID which contains no
     /// components.
     fn min_value() -> Self {
-        Self {
-            id: vec![],
-        }
+        Self { id: vec![] }
     }
 }
 
-impl<T> UpperBounded for GranularId<T> where T: UpperBounded {
+impl<T> UpperBounded for GranularId<T>
+where
+    T: UpperBounded,
+{
     /// The upper bound for any [`GranularId`] (the largest ID) is the empty ID which contains one
     /// component, containing the upper bound of the component type.
     fn max_value() -> Self {
@@ -128,18 +145,15 @@ impl<T> UpperBounded for GranularId<T> where T: UpperBounded {
 }
 
 impl<T> From<Vec<T>> for GranularId<T>
-    where
-        T: PartialEq + UpperBounded + Ord
+where
+    T: PartialEq + UpperBounded,
 {
     /// A vector of components may be turned into a `GranularId`. Keep in mind that the maximum
     /// `GranularId` of any type `T` is the one containing just the component `T::max`, attempting
     /// to convert any vector starting with `T::max` will result in the
     /// [`upper bound`](GranularId<T>::max_value) `GranularId` for that type.
     fn from(other: Vec<T>) -> Self {
-        if other
-            .first()
-            .map_or(false, |x| x == &T::max_value())
-        {
+        if other.first().map_or(false, |x| x == &T::max_value()) {
             Self {
                 id: vec![T::max_value()],
             }
@@ -149,16 +163,35 @@ impl<T> From<Vec<T>> for GranularId<T>
     }
 }
 
-impl<T> From<GranularId<T>> for Vec<T>
+impl<T> From<&[T]> for GranularId<T>
+where
+    T: PartialEq + UpperBounded + Clone,
 {
-    /// Creates a `GranularId` can be turned into a vector of its components.
+    /// A slice of components may be turned into a `GranularId`. Keep in mind that the maximum
+    /// `GranularId` of any type `T` is the one containing just the component `T::max`, attempting
+    /// to convert any vector starting with `T::max` will result in the
+    /// [`upper bound`](GranularId<T>::max_value) `GranularId` for that type.
+    fn from(other: &[T]) -> Self {
+        if other.first().map_or(false, |x| x == &T::max_value()) {
+            Self {
+                id: vec![T::max_value()],
+            }
+        } else {
+            Self { id: other.to_vec() }
+        }
+    }
+}
+
+impl<T> From<GranularId<T>> for Vec<T> {
+    /// Turns a `GranularId` into a vector of its components.
     fn from(other: GranularId<T>) -> Vec<T> {
         other.id
     }
 }
 
 impl<T> GranularId<T>
-    where T: SequentialId
+where
+    T: SequentialId,
 {
     /// Gets all the siblings of this `GranularId`, ordered from the smallest sibling to the largest
     /// sibling. This is all the `GranularId`s which starts the same as this `GranularId` but has
@@ -175,7 +208,9 @@ impl<T> GranularId<T>
     /// ```
     #[must_use]
     pub fn all_siblings(&self) -> SequentialIds<T>
-        where T: LowerBounded {
+    where
+        T: LowerBounded,
+    {
         let mut id = self.id.clone();
         id.pop();
         SequentialIds {
@@ -199,13 +234,12 @@ impl<T> GranularId<T>
     /// ```
     #[must_use]
     pub fn next_siblings(&self) -> SequentialIds<T>
-        where T: CheckedAdd {
+    where
+        T: CheckedAdd,
+    {
         let mut id = self.id.clone();
         let current = id.pop().and_then(|x| x.checked_add(&T::one()));
-        SequentialIds {
-            current,
-            base: id,
-        }
+        SequentialIds { current, base: id }
     }
 
     /// Gets all direct children of this `GranularId`, ordered from the smallest child (which is
@@ -222,7 +256,8 @@ impl<T> GranularId<T>
     /// assert_eq!(children.next().unwrap(), vec![1, 2, 3].into()); // id 1.2.3
     /// ```
     #[must_use]
-    pub fn children(&self) -> SequentialIds<T> where
+    pub fn children(&self) -> SequentialIds<T>
+    where
         T: LowerBounded,
     {
         SequentialIds {
@@ -233,7 +268,9 @@ impl<T> GranularId<T>
 }
 
 impl<T> GranularId<T>
-    where T: BackSequentialId {
+where
+    T: BackSequentialId,
+{
     /// Gets all the previous siblings of this `GranularId`, ordered from the largest sibling
     /// smaller than this ID to the smallest sibling. This is all the `GranularId`s which starts the
     /// same as this `GranularId` but has its last component changed to any smaller value.
@@ -249,18 +286,98 @@ impl<T> GranularId<T>
     /// ```
     #[must_use]
     pub fn previous_siblings(&self) -> BackSequentialIds<T>
-        where T: Bounded + One + Clone + CheckedSub
+    where
+        T: Bounded + One + Clone + CheckedSub,
     {
         let mut id = self.id.clone();
         let current = id.pop().and_then(|x| x.checked_sub(&T::one()));
-        BackSequentialIds {
-            current,
-            base: id,
-        }
+        BackSequentialIds { current, base: id }
     }
 }
 
 impl<T> GranularId<T> {
+    /// Checks whether this `GranularId` is the maximum value of this type. If it is, no
+    /// other larger `GranularId`s exist of the same type.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let not_max: GranularId<u8> = vec![254].into();
+    /// let max: GranularId<u8> = vec![255].into();
+    /// assert!(!not_max.is_max_value());
+    /// assert!(max.is_max_value());
+    /// ```
+    #[must_use]
+    pub fn is_max_value(&self) -> bool
+    where
+        T: UpperBounded + PartialEq,
+    {
+        self == &Self::max_value()
+    }
+
+    /// Gets the granularity of this `GranularId`, which is the number of components it contains,
+    /// or the "precision" it has
+    ///
+    /// ```rust
+    ///
+    /// use granular_id::GranularId;
+    /// let id: GranularId<u8> = vec![1, 2, 3, 4].into(); // id 1.2.3.4, granularity 4
+    /// assert_eq!(id.granularity(), 4);
+    /// // Its children should have granularity 5:
+    /// assert_eq!(id.children().next().unwrap().granularity(), 5);
+    /// ```
+    #[must_use]
+    pub fn granularity(&self) -> usize {
+        self.id.len()
+    }
+
+    /// Checks whether this `GranularId` is the root, which means not having any components.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let not_root: GranularId<u8> = vec![254].into();
+    /// let root: GranularId<u8> = vec![].into();
+    /// assert!(!not_root.is_root());
+    /// assert!(root.is_root());
+    /// ```
+    #[must_use]
+    pub fn is_root(&self) -> bool {
+        self.id.is_empty()
+    }
+
+    /// Turns this `GranularId` into its parent, that is, the same `GranularId` with the last
+    /// component removed. If this `GranularId` is the root (has no components), this function
+    /// returns the root. For a mutating version, see [`GranularId::pop`], and for a copying
+    /// version, see [`GranularId::parent`]
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// assert_eq!(id.into_parent(), vec![1, 2].into()); // id 1.2
+    /// ```
+    #[must_use]
+    pub fn into_parent(mut self) -> Self {
+        self.id.pop();
+        self
+    }
+
+    /// Gets the parent of this `GranularId`, that is, the same `GranularId` with the last component
+    /// removed.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// assert_eq!(id.parent(), vec![1, 2].into()); // id 1.2
+    /// ```
+    #[must_use]
+    pub fn parent(&self) -> Self
+    where
+        T: Clone,
+    {
+        let mut id = self.id.clone();
+        id.pop();
+        Self { id }
+    }
+
     /// Removes the last component of this `GranularId`. To get the parent of any `GranularId`
     /// without mutating it, see [`GranularId::parent`]
     ///
@@ -274,21 +391,140 @@ impl<T> GranularId<T> {
         self.id.pop();
     }
 
-    /// Gets the parent of this `GranularId`, that is, the same `GranularId` with the last component
-    /// removed.
+    /// Pushes a new component to the `GranularId`, adding it as a new last component. Since the
+    /// maximum bound for a `GranularId` of type `T` is `[T::max]`, if any component is pushed to
+    /// such an ID, the call doesn't do anything.
     ///
     /// ```rust
     /// use granular_id::GranularId;
-    /// let id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
-    /// assert_eq!(id.parent(), vec![1, 2].into()); // id 1.2
+    /// let mut id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// id.push(4);
+    /// assert_eq!(id, vec![1, 2, 3, 4].into()); // id 1.2.3.4
+    /// ```
+    pub fn push(&mut self, component: T)
+    where
+        T: UpperBounded + PartialEq,
+    {
+        if self != &Self::max_value() {
+            self.id.push(component);
+        }
+    }
+
+    /// Makes a new `GranularId`, adding the given component as its new last component. Since the
+    /// maximum bound for a `GranularId` of type `T` is `[T::max]`, if any component is pushed to
+    /// such an ID, the call returns a plain copy of the original `GranularId`.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let mut id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// assert_eq!(id.pushing(4), vec![1, 2, 3, 4].into()); // id 1.2.3.4
     /// ```
     #[must_use]
-    pub fn parent(&self) -> Self
-        where T: Clone {
-        let mut id = self.id.clone();
-        id.pop();
-        Self {
-            id
+    pub fn pushing(&self, component: T) -> Self
+    where
+        T: UpperBounded + PartialEq + Clone,
+    {
+        if self == &Self::max_value() {
+            self.clone()
+        } else {
+            let mut id = self.id.clone();
+            id.push(component);
+            Self { id }
+        }
+    }
+
+    /// Appends all components from another `GranularId` to this `GranularId`, adding them as a new
+    /// last components. Since the maximum bound for a `GranularId` of type `T` is `[T::max]`, if
+    /// any components is appended to such an ID, the call doesn't do anything.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let mut id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// let id_2 = vec![4, 5, 6].into(); // id 4.5.6
+    /// id.append(id_2);
+    /// assert_eq!(id, vec![1, 2, 3, 4, 5, 6].into()); // id 1.2.3.4.5.6
+    /// ```
+    pub fn append(&mut self, mut other: GranularId<T>)
+    where
+        T: UpperBounded + PartialEq,
+    {
+        if self != &Self::max_value() {
+            self.id.append(&mut other.id);
+        }
+    }
+
+    /// Makes a new `GranularId`, appending all components from another `GranularId` to that
+    /// `GranularId`, adding them as a new last components. Since the maximum bound for a
+    /// `GranularId` of type `T` is `[T::max]`, if any components is appended to such an ID, the
+    /// call returns a copy of the original `GranularId`.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let mut id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// let id_2 = vec![4, 5, 6].into(); // id 4.5.6
+    /// assert_eq!(id.appending(id_2), vec![1, 2, 3, 4, 5, 6].into()); // id 1.2.3.4.5.6
+    /// ```
+    #[must_use]
+    pub fn appending(&self, mut other: GranularId<T>) -> GranularId<T>
+    where
+        T: UpperBounded + PartialEq + Clone,
+    {
+        if self == &Self::max_value() {
+            self.clone()
+        } else {
+            let mut id = self.id.clone();
+            id.append(&mut other.id);
+            Self { id }
+        }
+    }
+
+    /// Appends all components from a `Vec` to this `GranularId`, adding them as a new
+    /// last components. Since the maximum bound for a `GranularId` of type `T` is `[T::max]`, if
+    /// any components is appended to such an ID, the call doesn't do anything.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let mut id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// id.append_vec(vec![4, 5, 6]);
+    /// assert_eq!(id, vec![1, 2, 3, 4, 5, 6].into()); // id 1.2.3.4.5.6
+    /// ```
+    pub fn append_vec(&mut self, mut other: Vec<T>)
+    where
+        T: UpperBounded + PartialEq,
+    {
+        if self.id.is_empty() {
+            // If this is empty, replace the internal vec, making sure to use into() to not
+            // exceed max
+            let other_id: GranularId<T> = other.into();
+            self.id = other_id.id;
+        } else if self != &Self::max_value() {
+            self.id.append(&mut other);
+        }
+    }
+
+    /// Makes a new `GranularId`, appending all components from a `Vec` to that
+    /// `GranularId`, adding them as a new last components. Since the maximum bound for a
+    /// `GranularId` of type `T` is `[T::max]`, if any components is appended to such an ID, the
+    /// call returns a copy of the original `GranularId`.
+    ///
+    /// ```rust
+    /// use granular_id::GranularId;
+    /// let mut id: GranularId<u8> = vec![1, 2, 3].into(); // id 1.2.3
+    /// assert_eq!(id.appending_vec(vec![4, 5, 6]), vec![1, 2, 3, 4, 5, 6].into()); // id 1.2.3.4.5.6
+    /// ```
+    #[must_use]
+    pub fn appending_vec(&self, mut other: Vec<T>) -> GranularId<T>
+    where
+        T: UpperBounded + PartialEq + Clone,
+    {
+        if self.id.is_empty() {
+            other.into()
+        } else if self != &Self::max_value() {
+            let mut id = self.id.clone();
+            id.append(&mut other);
+            Self { id }
+        } else {
+            self.clone()
         }
     }
 
@@ -306,6 +542,16 @@ impl<T> GranularId<T> {
     }
 }
 
+impl<T> IntoIterator for GranularId<T> {
+    type Item = T;
+    type IntoIter = <Vec<T> as IntoIterator>::IntoIter;
+
+    /// Turns this `GranularId` into an iterator over its components, consuming them.
+    fn into_iter(self) -> Self::IntoIter {
+        self.id.into_iter()
+    }
+}
+
 pub struct SequentialIds<T> {
     current: Option<T>,
     base: Vec<T>,
@@ -316,17 +562,13 @@ pub trait SequentialId: UpperBounded + One + Clone + PartialEq + Ord + CheckedAd
 impl<T: UpperBounded + One + Clone + PartialEq + Ord + CheckedAdd> SequentialId for T {}
 
 impl<T> Iterator for SequentialIds<T>
-    where
-        T: SequentialId,
+where
+    T: SequentialId,
 {
     type Item = GranularId<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self
-            .base
-            .first()
-            .map_or(false, |x| x >= &T::max_value())
-        {
+        if self.base.first().map_or(false, |x| x >= &T::max_value()) {
             return None;
         }
 
@@ -348,8 +590,8 @@ pub struct BackSequentialIds<T> {
 }
 
 impl<T> Iterator for BackSequentialIds<T>
-    where
-        T: BackSequentialId
+where
+    T: BackSequentialId,
 {
     type Item = GranularId<T>;
 
@@ -359,5 +601,39 @@ impl<T> Iterator for BackSequentialIds<T>
         id.push(x.clone());
         self.current = x.checked_sub(&T::one());
         Some(GranularId { id })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn readme_example() {
+        // Create a new GranularId from a vec of u8 (id: 1.2.3)
+        let id: GranularId<u8> = vec![1, 2, 3].into();
+
+        // Get the parent ID (id: 1.2)
+        let parent = id.parent();
+        assert_eq!(parent, vec![1, 2].into());
+
+        // Iterate over the following siblings of 1.2.3
+        let mut next_siblings = id.next_siblings();
+        // First one is 1.2.4
+        assert_eq!(next_siblings.next().unwrap(), vec![1, 2, 4].into());
+        // Then, 1.2.5, etc
+        assert_eq!(next_siblings.next().unwrap(), vec![1, 2, 5].into());
+        assert_eq!(next_siblings.next().unwrap(), vec![1, 2, 6].into());
+
+        // Get an iterator over childrens of 1.2.3
+        let mut children = id.children();
+        // First one is 1.2.3.0
+        assert_eq!(children.next().unwrap(), vec![1, 2, 3, 0].into());
+        // Then, 1.2.3.1, etc
+        assert_eq!(children.next().unwrap(), vec![1, 2, 3, 1].into());
+        assert_eq!(children.next().unwrap(), vec![1, 2, 3, 2].into());
+
+        // Each parent is always smaller than all of its children
+        assert!(parent < id);
     }
 }
